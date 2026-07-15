80119b894d7cf34a12c6e8d461db58e52ef867d0
tangle <2257739397@qq.com>
2026-07-15 09:10:01 +0800

    update data preprocess

--- data_preprocess.py
+++ data_preprocess.py
@@ -63,7 +63,7 @@ from IPython import get_ipython
 #     axes.set_ylim(ylim)
 #     if legend:
 #         axes.legend(legend)
-#     #set_axes函数设置matplotlib的轴的属性，包括x轴和y轴的标签、刻度、范围和图例。
+    #set_axes函数设置matplotlib的轴的属性，包括x轴和y轴的标签、刻度、范围和图例。
 
 
 #     #现在前期布置准备好了，我们就可以开始绘制函数图像了，下面我们来定义一个绘图函数plot
@@ -107,3 +107,67 @@ from IPython import get_ipython
 # #现在我们已经定义了绘图函数plot，并使用它绘制了函数f(x)和切线2x-3的图像。 
 # #想让他显示出来，我们需要调用d2l.plt.show()函数来显示图像。
 # d2l.plt.show()
+#----------------------------------------------------------------
+
+# #接下来学习一下自动微分
+x=torch.arange(4.0) #创建一个包含4个元素的张量，元素为0到3的整数
+
+x.requires_grad_(True) #设置x的requires_grad属性为True，表示需要计算梯度
+# y=2*torch.dot(x,x) 
+
+# y.backward()
+# #print(x.grad) #打印x的梯度，即dy/dx
+# #print(x.grad == 4 * x) #打印x的梯度是否等于4x
+
+# #接下来计算x的另一个函数
+# x.grad.zero_() #将x的梯度清零
+# y=x.sum()
+# y.backward()
+# print(x.grad) #打印x的梯度，即dy/dx
+
+#下一步，分离计算
+#有的时候，对于一个大模型，我们可能只想计算其中一部分的梯度，而不想计算整个模型的梯度，这时候就可以使用detach()方法来分离计算。
+#比如，对于一个骨干权重已经训练好的模型，我们可能只想计算最后一层的梯度，而不想计算前面几层的梯度，这时候就可以使用detach()方法来分离计算。
+# #这样做的好处是可以只训练并更新我们需要的层的梯度，不会影响其他层的梯度。
+# x.grad.zero_() #先将x的梯度清零
+# y = x * x 
+# u = y.detach() #在这里，我们使用detach()方法将y分离出来，得到一个新的张量u，它与y共享数据，但不再计算梯度，也就不会将y的梯度传递给x。
+# z = u * x
+
+# z.sum().backward()
+# x.grad == u 
+
+#-------------------------------------------------
+#概率
+from torch.distributions import multinomial
+
+#以掷骰子为例子，我们想要抽取一个样本，只需要输入一个概率分布向量和样本数量即可。
+#比如我们想要从一个6面骰子中抽取一个样本，概率分布向量为[1/6, 1/6, 1/6, 1/6, 1/6, 1/6]，样本数量为1，那么我们可以这样写：
+probs = torch.ones(6) / 6
+# output=multinomial.Multinomial(1, probs).sample() #输出一个样本，样本为一个one-hot向量，表示抽取的样本是哪一面骰子
+# print(output) #输出样本
+
+# #接下来我们想要抽取多个样本，比如我们想要从一个6面骰子中抽取10个样本，概率分布向量为[1/6, 1/6, 1/6, 1/6, 1/6, 1/6]，样本数量为10，那么我们可以这样写：
+# output1=multinomial.Multinomial(10, probs).sample() #输出10个样本，样本为一个one-hot向量，表示抽取的样本是哪一面骰子
+# print(output1) #输出样本
+
+# #好，那么接下来我们就可以模拟掷骰子的过程了，比如我们想要模拟掷骰子1000次，那么我们可以这样写：
+# output2=multinomial.Multinomial(1000, probs).sample() #输出1000个样本，样本为一个one-hot向量，表示抽取的样本是哪一面骰子
+# print(output2) #输出样本
+# #然后我们可以计算相对频率，看看是否接近理论概率1/6。比如我们想要计算每一面骰子出现的次数，那么我们可以这样写：
+# # relative_freq = output2 / 1000 #计算相对频率
+# # print(relative_freq) #输出相对频率
+
+# #下面，我们尝试进行500组模拟，每组模拟掷骰子1000次，看看每一面骰子出现的次数是否接近理论概率1/6。我们可以这样写：
+# counts = multinomial.Multinomial(10, probs).sample((500,)) #输出500组样本，每组样本为一个one-hot向量，表示抽取的样本是哪一面骰子
+# cum_counts = counts.cumsum(dim=0) #计算累计次数，cumsum()函数是对张量的元素进行累加，dim=0表示按行累加
+# estimates = cum_counts / cum_counts.sum(dim=1, keepdims=True) #计算相对频率，cum_counts.sum(dim=1, keepdims=True)表示按行求和，keepdims=True表示保持维度不变
+
+# d2l.set_figsize((6, 4.5))
+# for i in range(6):
+#     d2l.plt.plot(estimates[:, i].numpy(),
+#                  label=("P(die=" + str(i + 1) + ")"))
+# d2l.plt.axhline(y=0.167, color='black', linestyle='dashed')
+# d2l.plt.gca().set_xlabel('Groups of experiments')
+# d2l.plt.gca().set_ylabel('Estimated probability')
+# d2l.plt.legend();
